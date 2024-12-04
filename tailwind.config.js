d060c7a6657c7d40b583e8cd253976bd13085c16
Mohammad Shajalal <khanranu81@gmail.com>
2024-12-05 00:42:34 +0600

    header create 40 min

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color-primary': '#34bf49',
+        'color-secndary': '#2d465e',
+        'text-color': '#e1e1e2',
+        'color-white-bold': '#f3f4f4',
+        'color-border': '#F0F0F0',
+        'color-white-dark': '#FDF7FB',
+        'gradient-start': '#FFFFFF',
+        'gradient-end': '#D8A7BE',
+      },
+      fontFamily : {
+        'font-roboto': 'Roboto',
+        'font-lato': 'lato',
+      }
+    },
+    container : {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        md: '0px',
+      }
+    }
   },
   plugins: [],
 }
