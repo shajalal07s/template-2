1513ca09fe129fd37b13fae00d0c600fb2c8df7d
Mohammad Shajalal <khanranu81@gmail.com>
2024-12-08 13:25:55 +0600

    desgin customzation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         'color-white-bold': '#f3f4f4',
         'color-border': '#f2f2f2',
         'color-white-ass': '#cdcfcf',
+        'color-white-black': '#eaebec',
       },
       fontFamily : {
         'font-roboto': 'Roboto',
@@ -20,7 +21,7 @@ module.exports = {
       center: true,
       padding: {
         DEFAULT: '20px',
-        md: '0px',
+        md: '40px',
       }
     }
   },
